6abc2e4642e5eb06a60aa75b4fcd4c23fed83776
Aditya Vikram <adityavikram2016@gmail.com>
2023-03-21 10:54:15 -0400

    commands

--- cli/ComixCli.java
+++ cli/ComixCli.java
@@ -21,31 +21,11 @@ public class ComixCli {
             scanner.close();
             System.exit(0);
         } 
-
         ICliCmd cmd = cmdMap.get(commands[0]);
         if (cmd != null) {
             cmd.execute(commands);
             return;
         }
-
-        // else if (command.equals("logout")) {
-        //     proxy.logout();
-        //     ColorWriter.out("Logged out\n", ColorWriter.ANSI_GREEN);
-        // }
-        // else if (command.equals("doSomething")) {
-        //     String out = proxy.doSomething();
-        //     if (out != null) {
-        //         ColorWriter.out(out+"\n", ColorWriter.ANSI_WHITE); return;
-        //     }
-        //     ColorWriter.out("Not logged in\n", ColorWriter.ANSI_RED);
-        // } 
-        // else if (command.equals("help")) {
-        //     printHelp();
-        // } 
-        // else if (command.equals("exit")) {
-        //     scanner.close();
-        //     System.exit(0);
-        // } 
         throw new IllegalArgumentException("Unknown command \"" + commands[0] + "\"\n");
     }
 
