29ff6089f6db75089793972307cb3a9e40a39a9a
Andrew Apollo <andthetank@icloud.com>
2023-03-29 11:10:56 -0400

    Comic interface implemented

--- src/comic/Comic.java
+++ src/comic/Comic.java
@@ -1,5 +1,27 @@
 package comic;
 
-public class Comic {
+import java.util.List;
 
+public interface Comic {
+
+    public String getName();
+    
+    public String getPublisher();
+
+    public int getVolumeNumber();
+
+    public int getIssueNumber();
+
+    public String getPublicationDate();
+
+    public List<String> getCreators();
+
+    public List<String> getPrincipleCharacters();
+
+    public String getDescription();
+
+    public int getValue();
+
+    public void setValue(int value);
+    
 }
