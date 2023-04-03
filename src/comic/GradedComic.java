52363d228e4773aaf32f03961b41e0fe43f04a7c
Andrew Apollo <andthetank@icloud.com>
2023-04-03 11:27:33 -0400

    Concrete Comic, graded and slabbed commics implmemnted awaiting conformation that it was implemented correctly

--- src/comic/GradedComic.java
+++ src/comic/GradedComic.java
@@ -1,5 +1,26 @@
 package comic;
 
-public class GradedComic {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/*
+ * Defines what a graded comic can do 
+ * 
+ * @author Andrew Apollo 
+ */
+public class GradedComic extends ConcreteComic {
     
+    @JsonProperty("grade") private int Grade;
+   
+    public GradedComic(int Grade, Comic comic){
+        super(comic);
+        this.Grade = Grade;
+    }
+
+    public void addGrade(int Grade){
+        this.Grade = Grade;
+    }
+
+    public int getGrade(){
+        return Grade;
+    }
 }
--- src/comic/SlabbedComic.java
+++ src/comic/SlabbedComic.java
@@ -1,5 +1,21 @@
 package comic;
 
-public class SlabbedComic {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class SlabbedComic extends ConcreteComic {
     
+    @JsonProperty("grade") private boolean Slabbed;
+   
+    public SlabbedComic(boolean Slabbed, Comic comic){
+        super(comic);
+        this.Slabbed = Slabbed;
+    }
+
+    public void addSlab(boolean slab){
+        this.Slabbed = slab;
+    }
+
+    public boolean getSlab(){
+        return Slabbed;
+    }
 }
