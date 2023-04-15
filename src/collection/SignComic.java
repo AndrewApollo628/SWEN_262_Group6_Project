584869d4ca1b4570df07611c3b011785ef654f63
Josh Shaffer <jds7626@rit.edu>
2023-04-15 12:36:46 -0400

    Added grade, slab, sign commands

--- src/collection/SignComic.java
+++ src/collection/SignComic.java
@@ -1,8 +1,22 @@
 package collection;
 
-public class SignComic implements CollectionCommand{
-    @Override
-    public void execute() {
+import comic.Comic;
+import comic.SignedComic;
 
+public class SignComic implements CollectionCommand{
+
+    private Collection collection;
+    private Comic comic;
+
+    public SignComic(Collection collection, Comic comic){
+        this.collection = collection;
+        this.comic = comic;
+    }
+
+    @Override
+    public void execute(){
+        Comic signed = new SignedComic(this.comic);
+        this.collection.removeComic(this.comic);
+        this.collection.addComic(this.comic);
     }
 }
--- src/collection/SlabComic.java
+++ src/collection/SlabComic.java
@@ -1,8 +1,22 @@
 package collection;
 
+import comic.Comic;
+import comic.SlabbedComic;
+
 public class SlabComic implements CollectionCommand{
+
+    private Collection collection;
+    private Comic comic;
+
+    public SlabComic(Collection collection, Comic comic){
+        this.collection = collection;
+        this.comic = comic;
+    }
+
     @Override
     public void execute() {
-
+        Comic slabbed = new SlabbedComic(this.comic);
+        this.collection.removeComic(this.comic);
+        this.collection.addComic(slabbed);
     }
 }
